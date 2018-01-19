6101f23146cb8c4eabcc57ca571bc26f263cdad5
Gautam Jain <supergautam995@gmail.com>
2018-01-19 22:25:27 +0530

    more stuff

--- scripts/index.js
+++ scripts/index.js
@@ -168,3 +168,89 @@ $(function() {
   })
 
 });
+
+//smoth scroll on clicks
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+//smooth scroll on mouse
+
+// Math.easeOutQuad = function (t, b, c, d) { t /= d; return -c * t*(t-2) + b; };
+//
+// (function() { // do not mess global space
+// var
+//   interval, // scroll is being eased
+//   mult = 0, // how fast do we scroll
+//   dir = 0, // 1 = scroll down, -1 = scroll up
+//   steps = 50, // how many steps in animation
+//   length = 30; // how long to animate
+// function MouseWheelHandler(e) {
+//   e.preventDefault(); // prevent default browser scroll
+//   clearInterval(interval); // cancel previous animation
+//   ++mult; // we are going to scroll faster
+//   var delta = -Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))); // cross-browser
+//   if(dir!=delta) { // scroll direction changed
+//     mult = 1; // start slowly
+//     dir = delta;
+//   }
+//   // in this cycle, we determine which element to scroll
+//   for(var tgt=e.target; tgt!=document.documentElement; tgt=tgt.parentNode) {
+//     var oldScroll = tgt.scrollTop;
+//     tgt.scrollTop+= delta;
+//     if(oldScroll!=tgt.scrollTop) break;
+//     // else the element can't be scrolled, try its parent in next iteration
+//   }
+//   var start = tgt.scrollTop;
+//   var end = start + length*mult*delta; // where to end the scroll
+//   var change = end - start; // base change in one step
+//   var step = 0; // current step
+//   interval = setInterval(function() {
+//     var pos = Math.easeOutQuad(step++,start,change,steps); // calculate next step
+//     tgt.scrollTop = pos; // scroll the target to next step
+//     if(step>=steps) { // scroll finished without speed up - stop animation
+//       mult = 0; // next scroll will start slowly
+//       clearInterval(interval);
+//     }
+//   },10);
+// }
+//
+// // nonstandard: Chrome, IE, Opera, Safari
+// window.addEventListener("mousewheel", MouseWheelHandler, false);
+// // nonstandard: Firefox
+// window.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+// })();
