2cd1429d916a3e8b74f4d14506384ae8aa7e5ee5
Gautam Jain <supergautam995@gmail.com>
2018-01-20 12:31:03 +0530

    stuff

--- scripts/index.js
+++ scripts/index.js
@@ -208,6 +208,23 @@ $('a[href*="#"]')
     }
   });
 
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      //if (confirm('A new version of this app is available. Load it?')) {
+        window.location.reload();
+      //}
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
+
 //smooth scroll on mouse
 
 // Math.easeOutQuad = function (t, b, c, d) { t /= d; return -c * t*(t-2) + b; };
